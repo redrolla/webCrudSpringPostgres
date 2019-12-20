4db812655407d70b9405de3111aa6a44a9215296
Nick Altunin <redrolla1@gmail.com>
2019-12-20 20:31:28 +0300

    final nodb commit

--- src/main/java/testApplication/testApp/TestAppApplication.java
+++ src/main/java/testApplication/testApp/TestAppApplication.java
@@ -3,10 +3,17 @@ package testApplication.testApp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestAppApplication {
+public class TestAppApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestAppApplication.class);
 	}
 }
