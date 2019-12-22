21bbdf3e76a8fdf7f7119433002791be17eed806
Nick Altunin <redrolla1@gmail.com>
2019-12-22 14:25:34 +0300

    uncertan commit2

---
+++ src/main/java/testApplication/testApp/model/DataSourceConfiguration.java
@@ -0,0 +1,21 @@
+package testApplication.testApp.model;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource getDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/secondDB");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("katrin");
+        return dataSourceBuilder.build();
+    }
+}
