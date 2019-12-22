3d7692434e6f1486f77190057fb89182e00770ca
Nick Altunin <redrolla1@gmail.com>
2019-12-22 13:54:38 +0300

    uncertan commit

--- src/main/java/testApplication/testApp/TestAppApplication.java
+++ src/main/java/testApplication/testApp/TestAppApplication.java
@@ -1,11 +1,7 @@
 package testApplication.testApp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.ui.Model;
 
 @SpringBootApplication
 public class TestAppApplication {
