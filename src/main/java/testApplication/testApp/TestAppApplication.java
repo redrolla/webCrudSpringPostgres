e8e46669f6de94506fa025c930ea573c0b49d533
Nick Altunin <redrolla1@gmail.com>
2019-12-26 12:49:54 +0300

    works on local tomcat

--- src/main/java/testApplication/testApp/TestAppApplication.java
+++ src/main/java/testApplication/testApp/TestAppApplication.java
@@ -2,15 +2,17 @@ package testApplication.testApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestAppApplication {
-	/**
-	 * TODO
-	 *
-	 */
-
+public class TestAppApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestAppApplication.class);
 	}
 }
--- src/main/java/testApplication/testApp/model/DataSourceConfiguration.java
+++ src/main/java/testApplication/testApp/model/DataSourceConfiguration.java
@@ -3,14 +3,16 @@ package testApplication.testApp.model;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfiguration {
 
+
+
     @Bean
-    public DataSource getDataSource()
-    {
+    public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/secondDB");
