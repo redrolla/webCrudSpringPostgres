373229b53b9b77c3efd833c02471dcf3b8aa3d3c
Nick Altunin <redrolla1@gmail.com>
2019-12-30 12:46:24 +0300

    working DB-version for heroku

--- src/main/java/testApplication/testApp/model/DataSourceConfiguration.java
+++ src/main/java/testApplication/testApp/model/DataSourceConfiguration.java
@@ -10,7 +10,8 @@ import javax.sql.DataSource;
 public class DataSourceConfiguration {
 
 
-
+    /*
+    Local
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
@@ -18,6 +19,20 @@ public class DataSourceConfiguration {
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/secondDB");
         dataSourceBuilder.username("postgres");
         dataSourceBuilder.password("katrin");
+        return dataSourceBuilder.build();
+    }
+     */
+
+    /**
+     * Heroku
+     */
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/d7buvkel31u5vm");
+        dataSourceBuilder.username("cfakvxouhxjxaa");
+        dataSourceBuilder.password("54a2d3b89038bea0bc6e7a90c7844a6f69feca43c6aa1d1dcfe1e3221d231a2f");
         return dataSourceBuilder.build();
     }
 }
